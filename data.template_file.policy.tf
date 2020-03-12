7d44aa35dd7444be48dbeaf1bf13bc542f3b6ab0
Basheer <basheer.olorangara@urolime.com>
2020-03-12 17:08:22 +0530

    TF version upgraded to v0.12.19

--- data.template_file.policy.tf
+++ data.template_file.policy.tf
@@ -1,8 +1,9 @@
 data "template_file" "cloudtrail_policy" {
-  template = "${file("${path.module}/files/s3_cloudtrail_policy.json")}"
+  template = file("${path.module}/files/s3_cloudtrail_policy.json")
 
-  vars {
+  vars = {
     s3_bucket_name = "cloudtail-${data.aws_caller_identity.current.account_id}"
-    account_id     = "${data.aws_caller_identity.current.account_id}"
+    account_id     = data.aws_caller_identity.current.account_id
   }
 }
+
