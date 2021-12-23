dc48a95f59d47c34c92ca6e7bed7a7d9698bdcd7
jjcallis <jjcallis@users.noreply.github.com>
2021-12-23 17:06:18 +0000

    also create clodwatch role for arn
    fixes this error: Error: Error creating CloudTrail: InvalidCloudWatchLogsRoleArnException: You must specify a role ARN as well as a log group.

--- cloudtrail.tf
+++ cloudtrail.tf
@@ -5,9 +5,25 @@ resource "aws_cloudtrail" "cloudtrail" {
   include_global_service_events = true
   is_multi_region_trail         = true
   enable_log_file_validation    = true
+  enable_logging                = true
 
   # CloudTrail trails should have CloudWatch log integration enabled
+
+  cloud_watch_logs_role_arn  = aws_iam_role.cloud_trail.arn
   cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.log_group.arn}:*"
 
   depends_on = [module.cloudtrail_bucket]
+}
+
+resource "aws_iam_role" "cloud_trail" {
+  name = "cloudTrail-cloudWatch-role"
+
+  assume_role_policy = file("${path.module}/files/cloud_trail_assume_role.json")
+}
+
+resource "aws_iam_role_policy" "aws_iam_role_policy_cloudTrail_cloudWatch" {
+  name = "cloudTrail-cloudWatch-policy"
+  role = aws_iam_role.cloud_trail.id
+
+  policy = file("${path.module}/files/aws_iam_role_policy_cloudTrail_cloudWatch.json")
 }
