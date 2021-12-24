0b29157d8203acd69c9fe98728d478b4ea981dea
jjcallis <jjcallis@users.noreply.github.com>
2021-12-24 11:46:27 +0000

    remote file.json for aws_iam_policy_document

--- data.template_file.policy.tf
+++
@@ -1,9 +0,0 @@
-data "template_file" "cloudtrail_policy" {
-  template = file("${path.module}/files/s3_cloudtrail_policy.json")
-
-  vars = {
-    s3_bucket_name = "cloudtail-${data.aws_caller_identity.current.account_id}"
-    account_id     = data.aws_caller_identity.current.account_id
-  }
-}
-
--- module_cloudtrail_bucket.tf
+++ module_cloudtrail_bucket.tf
@@ -1,7 +1,7 @@
 module "cloudtrail_bucket" {
   source                              = "git::ssh://git@github.com/osodevops/aws-terraform-module-s3.git"
   s3_bucket_name                      = "cloudtail-${data.aws_caller_identity.current.account_id}"
-  s3_bucket_policy                    = data.template_file.cloudtrail_policy.rendered
+  s3_bucket_policy                    = data.aws_iam_policy_document.s3_bucket_policy.json
   s3_bucket_force_destroy             = true
   enable_lifecycle                    = false
   block_public_acls                   = true
@@ -10,4 +10,42 @@ module "cloudtrail_bucket" {
   restrict_public_buckets             = true
   s3_sse_algorithm                    = "aws:kms"
   common_tags                         = var.common_tags
+}
+data "aws_iam_policy_document" "s3_bucket_policy" {
+  statement {
+    sid = "AWSCloudTrailAclCheck20150319"
+    effect = "Allow"
+
+    principals {
+      type = "Service"
+
+      identifiers = [
+        "cloudtrail.amazonaws.com",
+      ]
+    }
+
+    actions = ["s3:GetBucketAcl"]
+    resource = ["arn:aws:s3::cloudtail-${data.aws_caller_identity.current.account_id}/*"]
+  }
+  statement {
+    sid = "AWSCloudTrailWrite20150319"
+    effect = "Allow"
+
+    principals {
+      type = "Service"
+
+      identifiers = [
+        "cloudtrail.amazonaws.com",
+      ]
+    }
+
+    actions = ["s3:PutObject"]
+    resource = ["arn:aws:s3::cloudtail-${data.aws_caller_identity.current.account_id}/*"]
+
+    condition {
+      test = "StringEquals"
+      variable = "s3:x-amz-acl"
+      values = ["bucket-owner-full-control"]
+    }
+  }
 }
